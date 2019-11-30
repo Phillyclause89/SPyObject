f2c3fa797ed6589ab5661b10af5a3f47803982d9
Phillyclause89 <45711864+Phillyclause89@users.noreply.github.com>
2019-11-30 15:05:09 -0800

    adding class struct

--- spyobject.py
+++ spyobject.py
@@ -1,41 +1,64 @@
-def obj_info(obj):
-    def get_obj_name(_obj, items=globals()):
+class SPyObject:
+    def __init__(self, instance, scope=globals()):
+        self.obj = instance
+        self.scope = scope
+        self.name = self.get_obj_name(self.scope)
+        self.type = type(self.obj)
+        self.docstr = self.obj.__doc__
+
+    def obj_info(self):
+        print(
+            f"{'_' * 75}\n\n"
+            f"\033[1;35mName Assignments:\033[1;m "
+            f"{self.name}"
+            f"\n\n\033[1;35mReturn Value:\033[1;m "
+            f"{self.obj}"
+            f"\n\n\033[1;35mReturn Type:\033[1;m "
+            f"{self.type}"
+            f"\n\n\033[1;35m__doc__ Text:\033[1;m "
+            f"{self.docstr}"
+            f"\n{'_' * 75}"
+        )
+
+    def get_obj_name(self, items=globals()):
+        names = []
         for key, x in items.items():
-            print(f"x:{x}, obj:{_obj}, key:{key}")
-            if x is _obj and key is not None:
-                return key
+            if x is self.obj and key is not None:
+                names.append(f"{key} {x}")
             if "<function" in str(x):
                 try:
-                    if str(_obj) == str(eval(f"{key}()")):
-                        return f"{key} {x}"
-                    if isinstance(_obj, pandas.core.groupby.generic.DataFrameGroupBy) and str(_obj.describe()) == str(
-                            eval(f"{key}()").describe()):
-                        return f"{key} {x}"
+                    if str(self.obj) == str(eval(f"{key}()")):
+                        names.append(f"{key} {x}")
+                    if isinstance(self.obj, pandas.core.groupby.generic.DataFrameGroupBy) and str(
+                            self.obj.describe()) == str(eval(f"{key}()").describe()):
+                        names.append(f"{key} {x}")
                 except TypeError:
                     pass
-            if "items" in dir(x) and len(x) > 0 and get_obj_name(_obj, items=x) is not None:
-                return get_obj_name(_obj, items=x)
-
-    name = get_obj_name(obj)
-    print(
-        f"{'_' * 75}\n\n"
-        f"\033[1;35mname:\033[1;m "
-        f"{name}"
-        f"\n\n\033[1;35mvalue:\033[1;m "
-        f"{obj}"
-        f"\n\n\033[1;35mtype:\033[1;m "
-        f"{type(obj)}"
-        f"\n\n\033[1;35m__doc__:\033[1;m "
-        f"{obj.__doc__}"
-        f"\n{'_' * 75}"
-    )
+                except NameError:
+                    pass
+            if "items" in dir(x) and len(x) > 0 and self.get_obj_name(items=x) is not None:
+                return self.get_obj_name(items=x)
+        return names
 
 
 if __name__ == "__main__":
-    # test = [False, True, 0, 1, 1000, 0.0006969, "P", "Python", [100, 50], (101, 202), {"key": 20}, ]
-    # obj_info(test)
-    # for item in test:
-    #     obj_info(item)
-    import random
+    def tester():
+        return 1
 
-    obj_info(random.randint(2000, 3000))
+    FucCaller = tester()
+    SPyObject(FucCaller).obj_info()
+
+    class Tester():
+        def __init__(self, X):
+            self.x = X
+
+        def showx(self):
+            return self.x
+
+    ClsCaller = Tester(5)
+    SPyObject(ClsCaller).obj_info()
+    ClsMetCaller = ClsCaller.showx()
+    SPyObject(ClsMetCaller).obj_info()
+    ClsAttrCaller = ClsCaller.x
+    SPyObject(ClsAttrCaller).obj_info()
+
