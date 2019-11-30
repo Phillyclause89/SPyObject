1fa0084e2c3ebf8415eefd4ee603673f71817f3b
Phillyclause89 <45711864+Phillyclause89@users.noreply.github.com>
2019-11-30 13:58:38 -0800

    setting up project

--- spyobject.py
+++ spyobject.py
@@ -1,13 +1,14 @@
 def obj_info(obj):
     def get_obj_name(_obj, items=globals()):
         for key, x in items.items():
+            print(f"x:{x}, obj:{_obj}, key:{key}")
             if x is _obj and key is not None:
                 return key
             if "<function" in str(x):
                 try:
                     if str(_obj) == str(eval(f"{key}()")):
                         return f"{key} {x}"
-                    if isinstance(_obj, pd.core.groupby.generic.DataFrameGroupBy) and str(_obj.describe()) == str(
+                    if isinstance(_obj, pandas.core.groupby.generic.DataFrameGroupBy) and str(_obj.describe()) == str(
                             eval(f"{key}()").describe()):
                         return f"{key} {x}"
                 except TypeError:
@@ -28,3 +29,13 @@ def obj_info(obj):
         f"{obj.__doc__}"
         f"\n{'_' * 75}"
     )
+
+
+if __name__ == "__main__":
+    # test = [False, True, 0, 1, 1000, 0.0006969, "P", "Python", [100, 50], (101, 202), {"key": 20}, ]
+    # obj_info(test)
+    # for item in test:
+    #     obj_info(item)
+    import random
+
+    obj_info(random.randint(2000, 3000))
